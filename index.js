2557f00cdeda94da496b0ef04e0b8f17df41b46e
WebDevAlok <bhushan.alok13@gmail.com>
2022-11-23 10:50:26 +0530

    Initialized database model

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
+const Campground = require('./models/Campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,'connection error:'));
+db.once("open", () => {
+    console.log('Database Connected');
+});
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
 
 app.get('/', (req,res) => {
     res.render('index');
+})
+
+app.get('/makeCampground', async (req,res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping'});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
