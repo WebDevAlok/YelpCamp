b21c52cb681d8edcb85511fe62e44847beb66ff3
WebDevAlok <bhushan.alok13@gmail.com>
2022-12-15 20:54:24 +0530

    Added Images to Show page

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -14,11 +14,14 @@ db.once("open", () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i<300; i++) {
+    for (let i = 0; i<5; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground( {
+            image: 'https://source.unsplash.com/collection/483251',
             title: `${ sample(descriptors) } ${ sample(places) }`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            price: `${10 + Math.floor(Math.random() * 20)}`,
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique iaculis sem. Aliquam sagittis imperdiet leo, sed rhoncus enim. In eget auctor nisl, ut malesuada nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla id risus a augue bibendum finibus vitae at elit. Mauris in mollis erat. Aenean at diam mattis, condimentum magna ut, faucibus lorem.',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
         })
         await camp.save();
         //Uncomment the next line to prints out the value of random1000 use for selecting cities from cities.js
