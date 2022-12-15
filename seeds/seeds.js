695038e395fafa0fd7f1070aae39857996ecd76a
WebDevAlok <bhushan.alok13@gmail.com>
2022-12-15 23:18:41 +0530

    Index Styling Done

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -11,16 +11,18 @@ db.once("open", () => {
     console.log('Database Connected');
 });
 
+const fetchImage = ['https://images.unsplash.com/photo-1502459225234-12ab69bd5f11?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY3MTEyNDIzNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080','https://images.unsplash.com/photo-1493515694075-fff2d464227d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY3MTEyNDM0OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080','https://images.unsplash.com/photo-1499618838676-f1a9bc076843?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY3MTEyNDQ4Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080','https://images.unsplash.com/photo-1504106379193-10da019d1e9e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY3MTEyNDQ5Nw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080','https://images.unsplash.com/photo-1496216342243-42635b603639?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODMyNTF8fHx8fHx8MTY3MTEyNDUyMA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080']
+
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i<5; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground( {
-            image: 'https://source.unsplash.com/collection/483251',
+            image: `${fetchImage[i]}`,
             title: `${ sample(descriptors) } ${ sample(places) }`,
             price: `${10 + Math.floor(Math.random() * 20)}`,
-            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique iaculis sem. Aliquam sagittis imperdiet leo, sed rhoncus enim. In eget auctor nisl, ut malesuada nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla id risus a augue bibendum finibus vitae at elit. Mauris in mollis erat. Aenean at diam mattis, condimentum magna ut, faucibus lorem.',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique iaculis sem. Aliquam sagittis imperdiet leo, sed rhoncus enim. In eget auctor nisl, ut malesuada nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
         })
         await camp.save();
